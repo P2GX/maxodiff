43eb6dda3904c155274f2c7d055fa29bb382b70e
mabeckwith <mbeckwith30@gmail.com>
2024-12-13 16:27:42 -0500

    Remove unused imports.

--- maxodiff-core/src/main/java/org/monarchinitiative/maxodiff/core/model/SamplePhenopacket.java
+++ maxodiff-core/src/main/java/org/monarchinitiative/maxodiff/core/model/SamplePhenopacket.java
@@ -3,8 +3,6 @@ package org.monarchinitiative.maxodiff.core.model;
 import org.monarchinitiative.phenol.ontology.data.TermId;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public record SamplePhenopacket(String id, List<TermId> presentHpoTermIds, List<TermId> excludedHpoTermIds,
                                 List<TermId> diseaseIds) {
