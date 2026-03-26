8cfa44f6d3862134c823a4e5eccdb3114033770d
mabeckwith <mbeckwith30@gmail.com>
2026-03-26 13:55:10 -0400

    Simplify implementations (#168).

--- maxodiff-core/src/main/java/org/monarchinitiative/maxodiff/core/analysis/refinement/MaxodiffResultNewImpl.java
+++
@@ -1,7 +0,0 @@
-package org.monarchinitiative.maxodiff.core.analysis.refinement;
-
-import org.monarchinitiative.maxodiff.core.analysis.RankedMaxoResult;
-
-record MaxodiffResultNewImpl(RankedMaxoResult rankedMaxoResult) implements MaxodiffResultNew {
-
-}
