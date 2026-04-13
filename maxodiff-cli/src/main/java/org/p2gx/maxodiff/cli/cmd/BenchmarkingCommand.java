fe87741560d35ddf3407a0c6b1d476f5310f7bae
mabeckwith <mbeckwith30@gmail.com>
2026-04-13 10:04:55 -0400

    Remove hard-coded path for benchmarking IC file.

--- maxodiff-cli/src/main/java/org/p2gx/maxodiff/cli/cmd/BenchmarkingCommand.java
+++ maxodiff-cli/src/main/java/org/p2gx/maxodiff/cli/cmd/BenchmarkingCommand.java
@@ -20,6 +20,7 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
@@ -69,7 +70,9 @@ public class BenchmarkingCommand extends DDxCommand {
         hpoFrequencies = context.createHpoFrequencies();
 
         this.phenomizer = new PhenomizerDDxEngine(context);
-        File icFile = new File("/Users/beckwm/IdeaProjects/maxodiff/data/term-to-ic.csv");
+        String currentDir = Paths.get("").toAbsolutePath().toString();
+        String icFileDir = String.join(File.separator, currentDir, "data", "term-to-ic.csv");
+        File icFile = new File(icFileDir);
         Map<TermId, Double> termToIcMap = getTermToIcMap(icFile);
 
         if (this.phenopacketPath != null && this.phenopacketPath.toFile().isFile()) {
